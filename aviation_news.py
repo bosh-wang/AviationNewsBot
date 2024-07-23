7b7ef6bfa6f141cce8e64872a529450ed7dcb7f0
pohsunwang <96949018+bosh-wang@users.noreply.github.com>
2024-07-23 07:21:40 +0000

    chore: Add Discord bot functionality for aviation news crawling

---
+++ aviation_news.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def crawl_news():
+
+    stories = []
+    urls = []
+
+    for i in range(36, 1, -1):
+        url = f'https://flyfttw.com/category/all/page/{i}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        paragraphs = soup.find_all(class_="entry-title ast-blog-single-element")
+
+        for j in range(len(paragraphs)-1, -1, -1):
+            story_name = paragraphs[j].text
+            a = paragraphs[j].find_all('a')
+            url = a[0].get('href')
+            stories.append(story_name)
+            urls.append(url)
+
+
+    url = 'https://flyfttw.com/category/all/'
+    
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    paragraphs = soup.find_all(class_="entry-title ast-blog-single-element")
+
+    for i in range(len(paragraphs)-1, -1, -1):
+        story_name = paragraphs[i].text
+        a = paragraphs[i].find_all('a')
+        url = a[0].get('href')
+        stories.append(story_name)
+        urls.append(url)
+
+    data = {'story' : stories,
+            'url' : urls,
+            'read' : False}
+    df = pd.DataFrame(data)
+    df.to_csv('all_news.csv', index=False)
+
+    return stories, urls
+
+def next_news():
+    df = pd.read_csv('all_news.csv')
+    for i in range(len(df)):
+        if not df['read'][i]:
+            df['read'][i] = True
+            df.to_csv('all_news.csv', index=False)
+            break
+    return df['story'][i], df['url'][i]
+    
+def latest_news():
+    df = pd.read_csv('all_news.csv')
+    last_element = df.iloc[-1]
+    return last_element['story'], last_element['url']
---
+++ main.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+import os
+
+from aviation_news import crawl_news, next_news, latest_news
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if msg.startswith("!hello"):
+        await message.channel.send(
+            "Hello this is Aviation News"
+        )
+
+    if msg.startswith("!allnews"):
+        stories, urls = crawl_news()
+        # for i in range(len(stories)):
+        #     await message.channel.send(
+        #         f"{stories[i]} - {urls[i]}"
+        #     )
+    if msg.startswith("!nextnews"):
+        story, url = next_news()
+        await message.channel.send(
+            f"{story} - {url}"
+        )
+    if msg.startswith("!latestnews"):
+        story, url = latest_news()
+        await message.channel.send(
+            f"{story} - {url}"
+        )
+
+client.run(TOKEN)
